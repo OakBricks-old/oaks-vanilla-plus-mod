e3067220367ab70734e9589207a32e3c3a4fe12d
OakBricks <oakbricks@gmail.com>
2021-06-18 12:01:01 -0500

    org.oakbricks.vanillaplusplus

---
+++ src/main/java/org/oakbricks/vanillaplusplus/OakVanillaPlus.java
@@ -0,0 +1,10 @@
+package org.oakbricks.vanillaplusplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OakVanillaPlus implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
